4539fce9a978e873d25cd7aed442ae17f75fb883
rafael.oliveira <rafael.oliveira@gerencianet.com.br>
2024-02-29 22:21:14 -0300

    Add architectures experiments

---
+++ VG/New_architectures/CNN/cnn_medium.py
@@ -0,0 +1,232 @@
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+from typing import Tuple, Type
+
+from json import JSONEncoder
+from collections import defaultdict
+from scipy.io import loadmat
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.utils.class_weight import compute_class_weight
+from tensorflow import keras
+from tensorflow.keras import layers
+from keras.utils import to_categorical
+
+
+def segmentation_signals(path: str,
+                         list_ecgs: list,
+                         size_beat_before: int,
+                         size_beat_after: int,
+                         set_name: str
+                         ) -> defaultdict:
+
+    dict_signals_II = defaultdict(list)  # dict to load beats
+
+    for file in list_ecgs:
+
+        struct = loadmat(os.path.join(path, set_name, file))  # loading the original file
+        data = struct["individual"][0][0]  # loading info of the signal
+
+        beat_peaks = data["anno_anns"]  # reading R-peak
+        beat_types = data["anno_type"]  # reading type of beat
+
+        ecg_II = data["signal_r"][:, 0]  # reading lead II
+
+        if file == "114.mat":
+            ecg_II = data["signal_r"][:, 1]
+
+        for peak, beat_type in zip(beat_peaks, beat_types):
+
+            beat_samples_II = []  # list to save samples of beat II
+            # half_beat = int(size_beat/2) #half of size beat
+
+            # if the position is before the begining or
+            # if the position is after the ending
+            # do nothing
+            if (peak - size_beat_before) < 0 or (peak + size_beat_after) > len(ecg_II):
+                continue
+
+            # if type of beat is different than this list, do nothing
+            if beat_type not in "NLRejAaJSVEFP/fUQ":
+                continue
+
+            # taking the samples of beat window
+            beat_samples_II = ecg_II[int(peak - size_beat_before): int(peak + size_beat_after)]
+
+            # taking the type of beat and saving in dict
+            if beat_type in "NLRej":
+                dict_signals_II["N"].append(beat_samples_II)
+            elif beat_type in "AaJS":
+                dict_signals_II["S"].append(beat_samples_II)
+            elif beat_type in "VE":
+                dict_signals_II["V"].append(beat_samples_II)
+
+    return dict_signals_II
+
+
+def sampling_windows_10_beats(signals_II: dict) -> dict:
+
+    select_N_beats_II = []
+
+    for index, beat_II in enumerate(signals_II["N"], 1):
+        if (index % 10) == 0:
+            select_N_beats_II.append(beat_II)
+
+    signals_II["N"] = select_N_beats_II
+
+    return signals_II
+
+
+def scaling_dataset(signals_II: dict) -> Tuple[np.asarray, np.asarray, np.array]:
+
+    ecg_list = []
+    ecg_labels = []
+    classes = ["N", "S", "V"]
+
+    for class_, beats in signals_II.items():
+        for beat in beats:
+            ecg_list.append(np.asarray(beat).reshape(-1, 1))
+            ecg_labels.append(classes.index(class_))
+
+    res = compute_class_weight(class_weight="balanced", classes=np.unique(ecg_labels), y=ecg_labels)
+    classes_weights = {}
+    [classes_weights.update({i: j}) for i, j in enumerate(res)]
+
+    X = np.asarray(ecg_list).astype(np.float32).reshape(-1, 280, 1)
+    y = np.asarray(ecg_labels).astype(np.float32).reshape(-1, 1)
+    y = to_categorical(y)
+
+    return X, y, classes_weights
+
+
+def building_model() -> Type[keras.Model]:
+
+    input_layer = keras.Input(shape=(280, 1))
+
+    x = layers.Conv1D(filters=32, kernel_size=3, strides=2, activation="relu", padding="same")(input_layer)
+    x = layers.BatchNormalization()(x)
+
+    x = layers.Conv1D(filters=64, kernel_size=3, strides=2, activation="relu", padding="same")(x)
+    x = layers.BatchNormalization()(x)
+
+    x = layers.Conv1D(filters=128, kernel_size=5, strides=2, activation="relu", padding="same")(x)
+    x = layers.BatchNormalization()(x)
+
+    x = layers.Conv1D(filters=256, kernel_size=5, strides=2, activation="relu", padding="same")(x)
+    x = layers.BatchNormalization()(x)
+
+    x = layers.Dropout(0.2)(x)
+
+    x = layers.Flatten()(x)
+
+    x = layers.Dense(4096, activation="relu")(x)
+    x = layers.Dropout(0.2)(x)
+
+    x = layers.Dense(2048, activation="relu", kernel_regularizer=keras.regularizers.L2())(x)
+    x = layers.Dropout(0.2)(x)
+
+    x = layers.Dense(1024, activation="relu", kernel_regularizer=keras.regularizers.L2())(x)
+    x = layers.Dropout(0.2)(x)
+    x = layers.Dense(128, activation="relu", kernel_regularizer=keras.regularizers.L2())(x)
+
+    output_layer = layers.Dense(3, activation="softmax")(x)
+
+    return keras.Model(inputs=input_layer, outputs=output_layer)
+
+
+def plot_history_metrics(history: Type[keras.callbacks.History]) -> None:
+
+    for key, value in history.history.items():
+        plt.figure(figsize=(12, 6))
+        plt.plot(range(len(value)), value)
+        plt.title(str(key))
+        plt.savefig(f"./images/cnn_medium/{str(key)}.png",
+                    dpi=600)
+
+
+class NumpyFloatValuesEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.float32):
+            return float(obj)
+        return JSONEncoder.default(self, obj)
+
+
+def training_testing(X_train: np.asarray, y_train: np.asarray, X_val: np.asarray, y_val: np.asarray,
+                     classes_weight: np.array) -> None:
+
+    epochs = 150
+    callbacks = [
+        keras.callbacks.ReduceLROnPlateau(monitor="categorical_accuracy", factor=0.2, patience=5, min_lr=0.000001),
+        keras.callbacks.EarlyStopping(monitor="loss", patience=10, verbose=1)
+    ]
+
+    optimizer = keras.optimizers.Adam(amsgrad=True, learning_rate=0.001)
+    loss = keras.losses.CategoricalCrossentropy()
+    metrics = [keras.metrics.CategoricalAccuracy(),  keras.metrics.AUC(),  keras.metrics.Precision(),
+               keras.metrics.Recall()]
+
+    model = building_model()
+
+    model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
+
+    model_history = model.fit(x=X_train, y=y_train, epochs=epochs, callbacks=callbacks,
+                              class_weight=classes_weight, verbose=1)
+
+    plot_history_metrics(model_history)
+
+    loss, accuracy, auc, precision, recall = model.evaluate(x=X_val, y=y_val, verbose=1)
+
+    prediction_proba = model.predict(X_val)
+    prediction = np.argmax(prediction_proba, axis=1)
+    y_true = np.argmax(y_val, axis=1)
+    sns.heatmap(confusion_matrix(y_true, prediction),
+                xticklabels=["N", "S", "V"],
+                yticklabels=["N", "S", "V"],
+                annot=True,
+                fmt=".0f",
+                cmap="rocket_r")
+    plt.savefig(f"./images/cnn_medium/confusion_matrix_cnn_medium.png", dpi=600)
+
+    with open("./images/cnn_medium/report_cnn_medium.txt", "w") as f:
+        f.write(classification_report(y_true, prediction, zero_division=0))
+
+    with open("./images/cnn_medium/cnn_medium.txt", "w") as f:
+        f.write("TRAIN\n")
+        f.write(f"Loss: {np.array(model_history.history['loss']).mean()}\n")
+        f.write(f"Precision: {np.array(model_history.history['precision']).mean()}\n")
+        f.write(f"Recall: {np.array(model_history.history['recall']).mean()}\n")
+        f.write(f"AUC: {np.array(model_history.history['auc']).mean()}\n")
+        f.write(f"Accuracy: {np.array(model_history.history['categorical_accuracy']).mean()}\n")
+
+        f.write("\nTEST\n")
+        f.write(f"Loss: {loss}\n")
+        f.write(f"Precision: {precision}\n")
+        f.write(f"Recall: {recall}\n")
+        f.write(f"AUC: {auc}\n")
+        f.write(f"Accuracy: {accuracy}\n")
+
+
+if __name__ == "__main__":
+
+    # performing Early_Stopping to find the best number of epoch to training ML models
+
+    PATH = "../../..Data"
+    FILES = os.listdir(os.path.join(PATH, "Train"))
+    FILES_VAL = ["109.mat", "114.mat", "207.mat", "223.mat"]
+    FILES_TRAIN = list(set(FILES)-set(FILES_VAL))
+
+    print("segmentating...")
+    train_signals_II = segmentation_signals(PATH, FILES_TRAIN, 100, 180, "Train")
+    val_signals_II = segmentation_signals(PATH, FILES_VAL, 100, 180, "Train")
+
+    print("sampling...")
+    train_signals_II = sampling_windows_10_beats(train_signals_II)
+    val_signals_II = sampling_windows_10_beats(val_signals_II)
+
+    print("scaling")
+    X_train, y_train, classes_weights = scaling_dataset(train_signals_II)
+    X_test, y_test, _ = scaling_dataset(val_signals_II)
+
+    print("training and testing")
+    training_testing(X_train, y_train, X_test, y_test, classes_weights)
---
+++ VG/New_architectures/GCN/GCN120.py
@@ -0,0 +1,363 @@
+import os
+import numpy as np
+import pandas as pd
+import dgl
+import torch as th
+import torch.nn as nn
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+import seaborn as sns
+import igraph as ig
+
+from ts2vg import NaturalVG
+from dgl.data import DGLDataset
+from dgl.nn import SAGEConv
+from dgl.dataloading import GraphDataLoader
+from collections import defaultdict
+from scipy.io import loadmat
+from sklearn.metrics import classification_report, confusion_matrix
+from typing import Type, Tuple
+from sklearn.preprocessing import minmax_scale
+
+
+def segmentation_signals(path: str, list_ecgs: list, size_beat_before: int, size_beat_after: int, set_name: str
+                         ) -> Tuple[defaultdict, defaultdict]:
+
+    dict_signals_V1 = defaultdict(list)  # dict to load beats V1
+    dict_signals_II = defaultdict(list)  # dict to load beats II
+
+    for file in list_ecgs:
+
+        struct = loadmat(os.path.join(path, set_name, file))  # loading the original file
+        data = struct["individual"][0][0]  # loading info of the signal
+        ecg_V1 = data["signal_r"][:, 1]  # reading lead V1
+        ecg_II = data["signal_r"][:, 0]  # reading lead II
+
+        if file == '114.mat':
+            ecg_V1 = data['signal_r'][:, 0]
+            ecg_II = data['signal_r'][:, 1]
+
+        beat_peaks = data["anno_anns"]  # reading R-peak
+        beat_types = data["anno_type"]  # reading type of beat
+
+        for (peak, beat_type) in zip(beat_peaks, beat_types):
+
+            beat_samples_V1 = []  # list to save samples of beat V1
+            beat_samples_II = []  # list to save samples of beat II
+            # half_beat = int(size_beat/2) #half of size beat
+
+            # if the position is before the begining or
+            # if the position is after the ending
+            # do nothing
+
+            if (peak - size_beat_before) < 0 or (peak + size_beat_after) > len(ecg_V1):
+                continue
+
+            # if type of beat is different than this list, do nothing
+            if beat_type not in "NLRejAaJSVEFP/fUQ":
+                continue
+
+            # taking the samples of beat window
+            beat_samples_V1 = ecg_V1[int(peak - size_beat_before): int(peak + size_beat_after)]
+            beat_samples_II = ecg_II[int(peak - size_beat_before): int(peak + size_beat_after)]
+
+            # taking the type of beat and saving in dict
+            if beat_type in "NLRej":
+                dict_signals_V1["N"].append(beat_samples_V1)
+                dict_signals_II["N"].append(beat_samples_II)
+            elif beat_type in "AaJS":
+                dict_signals_V1["S"].append(beat_samples_V1)
+                dict_signals_II["S"].append(beat_samples_II)
+            elif beat_type in "VE":
+                dict_signals_V1["V"].append(beat_samples_V1)
+                dict_signals_II["V"].append(beat_samples_II)
+
+    return dict_signals_V1, dict_signals_II
+
+
+def sampling_windows_10_beats(signals: dict) -> dict:
+
+    select_N_beats = []
+
+    for index, beat in enumerate(signals['N'], 1):
+        if (index % 10) == 0:
+            select_N_beats.append(beat)
+
+    signals["N"] = select_N_beats
+
+    return signals
+
+
+def get_beats_features(signals_V1: dict, signals_II: dict) -> dict:
+
+    features = {}
+    nodes_feat = []
+    graph_it = 0
+
+    # iterate in dict of beats, rr-interval
+    for (_, beats_V1), (_, beats_II) in zip(signals_V1.items(), signals_II.items()):
+        for beat_V1, beat_II in zip(beats_V1, beats_II):
+            nodes_feat = []
+            [nodes_feat.append(minmax_scale([i, j, k])) for i, (j, k) in enumerate(zip(beat_II, beat_V1))]
+            features.update({graph_it: nodes_feat})
+            graph_it += 1
+
+    return features
+
+
+def convert_beats_in_graphs(signals_II: dict) -> Tuple[pd.DataFrame, pd.DataFrame]:
+
+    graph_id = []
+    graph_src = []
+    graph_dst = []
+    graph_nodes = []
+    graph_label = []
+    classes = ["N", "S", "V"]
+    graph_it = 0
+
+    for class_, beats in signals_II.items():
+        for beat in beats:
+            label = classes.index(class_)
+            g = NaturalVG(directed=None).build(beat)
+            G = g.as_igraph()
+            source_nodes_ids = [i[0] for i in ig.Graph.get_edgelist(G)]
+            destination_nodes_ids = [i[1] for i in ig.Graph.get_edgelist(G)]
+            num_nodes = G.vcount()
+            graph_id.extend([graph_it] * len(ig.Graph.get_edgelist(G)))
+            graph_src.extend(source_nodes_ids)
+            graph_dst.extend(destination_nodes_ids)
+            graph_nodes.extend([num_nodes] * len(ig.Graph.get_edgelist(G)))
+            graph_label.extend([label] * len(ig.Graph.get_edgelist(G)))
+            graph_it += 1
+            del G
+
+    edges = pd.DataFrame({"graph_id": graph_id, "src": graph_src, "dst": graph_dst})
+    properties = pd.DataFrame({"graph_id": graph_id, "label": graph_label, "num_nodes": graph_nodes})
+    properties.drop_duplicates(inplace=True)
+
+    del graph_id, graph_src, graph_dst, graph_nodes, graph_label
+
+    return edges, properties
+
+
+def plotting_acc_loss(acc_values: list, loss_values: list, epochs: int) -> None:
+
+    _, ax = plt.subplots(1, 2, figsize=(12, 6))
+    plt.suptitle("Treinamento", fontsize=20)
+    ax[0].plot(range(1, epochs+1), acc_values, "r")
+    ax[1].plot(range(1, epochs+1), loss_values, "b")
+    ax[0].set_title("Acurácia")
+    ax[1].set_title("Perda")
+    ax[0].set_xlabel("Épocas")
+    ax[1].set_xlabel("Épocas")
+    ax[0].set_ylabel("Acurácia")
+    ax[1].set_ylabel("Perda")
+    plt.savefig(f"./Images/acc_loss_gcn120.png", dpi=600)
+
+    return None
+
+
+def plotting_confusion_matrix(true_label: np.array, pred_label: np.array) -> None:
+
+    _, ax = plt.subplots(figsize=(20, 9))
+    matrix = confusion_matrix(true_label, pred_label)
+
+    sns.heatmap(
+        matrix,
+        ax=ax,
+        xticklabels=["N", "S", "V"],
+        yticklabels=["N", "S", "V"],
+        annot=True,
+        fmt=".0f",
+        cmap="rocket_r",
+    )
+
+    ax.set_title(f"Matriz de Confusão", fontsize=18)
+    ax.set_xlabel("Predição", fontsize=16)
+    ax.set_ylabel("Verdadeiro", fontsize=16)
+    plt.savefig(f"./Images/confusion_matrix_gcn120.png", dpi=600)
+
+    return None
+
+
+def getting_classification_report(true_label: np.array, pred_label: np.array, set_name: str):
+
+    with open(f"./Images/report_gcn120.txt", "w") as f:
+        f.write(set_name)
+        f.write("\n")
+        f.write(classification_report(true_label, pred_label, zero_division=0))
+        f.write("\n")
+
+    return None
+
+
+def divide_into_batches(dataset: Type[th.utils.data.Dataset]) -> Type[dgl.dataloading.GraphDataLoader]:
+
+    dataloader = GraphDataLoader(dataset, batch_size=64, drop_last=False, shuffle=True)
+
+    return dataloader
+
+
+def training(dataset_train: Type[dgl.data.DGLDataset], dataset_val: Type[dgl.data.DGLDataset]) -> None:
+
+    epochs = 150
+    pred_train_label = []
+    true_train_label = []
+    pred_val_label = []
+    true_val_label = []
+    acc_train = []
+    loss_train = []
+    num_correct_train = 0
+    num_vals_train = 0
+    num_correct_val = 0
+    num_vals_val = 0
+    tag = True
+
+    train_loader = divide_into_batches(dataset_train)
+    val_loader = divide_into_batches(dataset_val)
+
+    model = GCN(3, 120, 3)  # (n_nodes_features, n_nodes_hidden_layer, n_classes)
+
+    # creating the optimizer
+    opt = th.optim.Adam(model.parameters(), lr=0.001)
+
+    # executing the training step
+    for epoch in range(epochs):
+        for batched_graph, labels in train_loader:
+            pred = model(batched_graph, batched_graph.ndata["attr"].float())
+            if tag:
+                pred_train_label.extend(pred.argmax(1))
+                true_train_label.extend(labels)
+            num_correct_train += (pred.argmax(1) == labels).sum().item()
+            num_vals_train += len(labels)
+            loss = F.cross_entropy(pred, labels)
+            opt.zero_grad()
+            loss.backward()
+            opt.step()
+        tag = False
+        loss_train.append(loss)
+        acc_train.append(num_correct_train / num_vals_train)
+        print(f"epoch_{epoch}...")
+
+    for batched_graph, labels in val_loader:
+        pred = model(batched_graph, batched_graph.ndata["attr"].float())
+        num_correct_val += (pred.argmax(1) == labels).sum().item()
+        num_vals_val += len(labels)
+        pred_val_label.extend(pred.argmax(1))
+        true_val_label.extend(labels)
+
+    loss_train = [float(x.detach().numpy()) for x in loss_train]
+
+    # plotting the metrics
+    plotting_acc_loss(acc_train, loss_train, epochs)
+    plotting_confusion_matrix(true_val_label, pred_val_label)
+    getting_classification_report(true_val_label, pred_val_label, "VALIDATION")
+
+    return None
+
+
+class SyntheticDataset(DGLDataset):
+    def __init__(self, attr_edges, attr_properties, attr_features):
+        self.attr_edges = attr_edges
+        self.attr_properties = attr_properties
+        self.attr_features = attr_features
+
+        super().__init__(name="synthetic")
+
+    def process(self):
+
+        self.graphs = []
+        self.labels = []
+
+        # Create a graph for each graph ID from the edges table.
+        # First process the properties table into two dictionaries with graph IDs as keys.
+        # The label and number of nodes are values.
+        label_dict = {}
+        num_nodes_dict = {}
+        for _, row in self.attr_properties.iterrows():
+            label_dict[row["graph_id"]] = row["label"]
+            num_nodes_dict[row["graph_id"]] = row["num_nodes"]
+
+        # For the edges, first group the table by graph IDs.
+        edges_group = self.attr_edges.groupby("graph_id")
+
+        # For each graph ID...
+        for graph_id in edges_group.groups:
+            # Find the edges as well as the number of nodes and its label.
+            edges_of_id = edges_group.get_group(graph_id)
+            src = edges_of_id["src"].to_numpy()
+            dst = edges_of_id["dst"].to_numpy()
+            num_nodes = num_nodes_dict[graph_id]
+            label = label_dict[graph_id]
+
+            # Create a graph and add it to the list of graphs and labels.
+            g = dgl.graph((src, dst), num_nodes=num_nodes)
+            g.ndata["attr"] = th.tensor(np.array(self.attr_features[graph_id]))
+            g = dgl.add_self_loop(g)
+            self.graphs.append(g)
+            self.labels.append(label)
+            del g
+
+        # Convert the label list to tensor for saving.
+        self.labels = th.LongTensor(self.labels)
+
+    # get a graph and its label by index
+    def __getitem__(self, i):
+        return self.graphs[i], self.labels[i]
+
+    # number of graphs in the dataset
+    def __len__(self):
+        return len(self.graphs)
+
+
+class GCN(nn.Module):
+    def __init__(self, in_feats, h_feats, num_classes):
+        super(GCN, self).__init__()
+        self.conv1 = SAGEConv(in_feats, h_feats, "mean")
+        self.conv2 = SAGEConv(h_feats, h_feats - 80, "mean")
+        self.conv3 = SAGEConv(h_feats - 80, h_feats - 100, "mean")
+        self.conv4 = SAGEConv(h_feats - 100, num_classes, "mean")
+
+    def forward(self, g, in_feat):
+        h = self.conv1(g, in_feat)
+        h = F.relu(h)
+        h = self.conv2(g, h)
+        h = F.relu(h)
+        h = self.conv3(g, h)
+        h = F.relu(h)
+        h = self.conv4(g, h)
+        g.ndata["h"] = h
+        return dgl.mean_nodes(g, "h")
+
+
+if __name__ == '__main__':
+
+    PATH = "../../..Data"
+    FILES = os.listdir(os.path.join(PATH, 'Train'))
+    FILES_VAL = ['109.mat', '114.mat', '207.mat', '223.mat']
+    FILES_TRAIN = list(set(FILES)-set(FILES_VAL))
+
+    print("segmentating...")
+    train_signals_V1, train_signals_II = segmentation_signals(PATH, FILES_TRAIN, 100, 180, 'Train')
+    val_signals_V1, val_signals_II = segmentation_signals(PATH, FILES_VAL, 100, 180, 'Train')
+
+    print("sampling...")
+    train_signals_V1 = sampling_windows_10_beats(train_signals_V1)
+    train_signals_II = sampling_windows_10_beats(train_signals_II)
+    val_signals_V1 = sampling_windows_10_beats(val_signals_V1)
+    val_signals_II = sampling_windows_10_beats(val_signals_II)
+
+    print("extracting attributes...")
+    train_features = get_beats_features(train_signals_V1, train_signals_II)
+    val_features = get_beats_features(val_signals_V1, val_signals_II)
+
+    print("converting beats into graphs...")
+    train_edges, train_properties = convert_beats_in_graphs(train_signals_II)
+    val_edges, val_properties = convert_beats_in_graphs(val_signals_II)
+
+    print("creating dataset...")
+    dataset_train = SyntheticDataset(train_edges, train_properties, train_features)
+    dataset_val = SyntheticDataset(val_edges, val_properties, val_features)
+
+    print("training...")
+    training(dataset_train, dataset_val)
